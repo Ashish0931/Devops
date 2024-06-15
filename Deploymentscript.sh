94fbd774576a20b583a30ecf1e2b5fe66e22ad32
ashish <chaitanya.ashish1@gmail.com>
2024-06-15 23:19:58 +0000

    .

---
+++ Deploymentscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#this is a scrip to deploy the python code present in the devops folder."
+
+#---------------------------------------------------------------------------
+
+# changing path to working folder
+
+ cd /Python-project/Docker-Zero-to-Hero/examples/python-web-app
+
+ #building docker image 
+
+ Docker build -t 
+
